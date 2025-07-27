d3103a98d46bbbd50887fef98f83c2fb1f8d6804
赵芷若 <1512619344@qq.com>
2025-07-27 19:35:15 +0800

    删除一些默认页

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -19,29 +19,8 @@ export default navbar([
           "4",
         ],
       },
-      {
-        text: "香蕉",
-        icon: "pen-to-square",
-        prefix: "banana/",
-        children: [
-          {
-            text: "香蕉 1",
-            icon: "pen-to-square",
-            link: "1",
-          },
-          {
-            text: "香蕉 2",
-            icon: "pen-to-square",
-            link: "2",
-          },
-          "3",
-          "4",
-        ],
-      },
       { text: "樱桃", icon: "pen-to-square", link: "cherry" },
       { text: "火龙果", icon: "pen-to-square", link: "dragonfruit" },
-      "tomato",
-      "strawberry",
     ],
   },
   {
