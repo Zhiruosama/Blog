d37dabe93f3bae723431b0dfacbb72b6bf9429fe
赵芷若 <1512619344@qq.com>
2026-05-10 19:31:39 +0800

    更新标题

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -2,4 +2,12 @@ import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
   "/",
+  {
+    text: "博文",
+    icon: "pen-to-square",
+    prefix: "/posts/",
+    children: [
+      { text: "海道教育实习", link: "海道教育实习/" },
+    ],
+  },
 ]);
